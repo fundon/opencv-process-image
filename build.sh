5b74caf0524acc2288247db0703576e22f8b476b
Fangdun Cai <cfddream@gmail.com>
2017-04-06 17:47:24 +0800

    add MyGaussianFilter

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake gamma_correction
+rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake process_image
 cmake -D CMAKE_BUILD_TYPE=RELEASE .
 make -j2
 
